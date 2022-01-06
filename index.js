3b411de35cf9151f813398f3a1569c4cc309348c
anjanakavindu97 <anjanakavinducampus@gmail.com>
2022-01-07 02:13:18 +0530

    I added /add route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 //import express module
 const express = require('express')
+const res = require('express/lib/response')
 
 //create application object
 const app = express()
@@ -13,6 +14,12 @@ app.get('/',(req,res)=>{
     res.sendStatus(200)
 })
 
+//GetMapping add two numbers
+app.get('/add/:num1/num2' ,(req,rec)=>{
+    const{num1,num2} = req.params
+    const ans = num1 + num2;
+    res.send(ans)
+})
 //called app.listen method
 app.listen(port,()=>{
     console.log('Server is start up')
